93639c9b9c92d2c1cc1a97af66045ed0293e3e98
Patricia <patysiq@yahoo.com.br>
2021-03-15 11:46:38 -0300

    add new scenery

---
+++ GoLarryGo/Entity-Component/Scenary/Scenary.swift
@@ -0,0 +1,26 @@
+//
+//  Scenary.swift
+//  GoLarryGo
+//
+//  Created by PATRICIA S SIQUEIRA on 15/03/21.
+//
+
+import Foundation
+import SpriteKit
+
+class Scenery {
+    
+    
+    func moveScenery(_ size: CGSize) -> SKAction {
+        let moveBack = SKAction.moveBy(x: -size.width, y: 0, duration: 8)
+        let repositionBack = SKAction.moveBy(x: size.width, y: 0, duration: 0)
+        let loopBack = SKAction.repeatForever(SKAction.sequence([moveBack,repositionBack]))
+        return loopBack
+    }
+    
+    func moveSun(_ size: CGSize) -> SKAction {
+        let movedown = SKAction.move(to: CGPoint(x: 0, y: 0), duration: 20)
+        let loopDown = SKAction.repeatForever(movedown)
+        return loopDown
+    }
+}
---
+++ GoLarryGo/Entity-Component/Scenary/ZPositions.swift
@@ -0,0 +1,26 @@
+//
+//  ZPositions.swift
+//  GoLarryGo
+//
+//  Created by PATRICIA S SIQUEIRA on 15/03/21.
+//
+
+import Foundation
+import SpriteKit
+
+
+enum ZPositionsCategories {
+    static let background:         CGFloat = -6
+    static let sun:                CGFloat = -5
+    static let clounds:            CGFloat = -4
+    static let montainAlt:         CGFloat = -3
+    static let montainFix:         CGFloat = -2
+    static let ground:             CGFloat = -1
+}
+
+// cores
+extension UIColor {
+    static var back: UIColor { return UIColor(red: 210/255, green: 138/255, blue: 134/255, alpha: 1)
+    }
+}
+
