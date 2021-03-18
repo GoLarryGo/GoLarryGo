42b6a360a0d4a6b2d77851a0a00d79d9ffcbde3a
Jhennyfer Rodrigues de Oliveira <jhennyfer519@gmail.com>
2021-03-18 10:56:17 -0300

    delegating button functions

--- GoLarryGo/Controller/PauseViewController.swift
+++ GoLarryGo/Controller/PauseViewController.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+import UIKit
+import SpriteKit
+
+class PauseViewController: UIViewController {
+    
+    override func loadView() {
+        super.loadView()
+        let pauseView = PauseView()
+        pauseView.controller = self
+        pauseView.delegate = self
+        self.view = pauseView
+    }
+
+}
+
+extension PauseViewController: PauseViewButtonActionsDelegate {
+    
+    func soundButtonAction(sender: UIButton) {
+        print("a")
+    }
+    
+    func menuButtonAction(sender: UIButton) {
+//       go to menu
+        print("b")
+    }
+
+    func resumeButtonAction(sender: UIButton) {
+//     go back to the game
+        print("c")
+    }
+
+    func closeButtonAction(sender: UIButton) {
+//       go back to the game
+        print("d")
+    }
+
+    
+}
+
