1e27cb25cfe6d06275db3ed97955345d893c448a
Jhennyfer Rodrigues de Oliveira <jhennyfer519@gmail.com>
2021-03-24 09:12:58 -0300

    change sound button image and change button state in user defaults

--- GoLarryGo/Shared/SoundButtonHelper.swift
+++ GoLarryGo/Shared/SoundButtonHelper.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+import UIKit
+
+class SoundButtonHelper {
+    static let sharedSoundButtonState = SoundButtonHelper()
+    
+    func setButtonImage(button: UIButton) {
+        if UserDefaultsConfiguration.sharedUserDefaultConfig.isSoundOn() {
+            button.setImage(UIImage(named: "sound on"), for: .normal)
+        } else {
+            button.setImage(UIImage(named: "sound off"), for: .normal)
+        }
+  
+    }
+    
+    func changeSoundButtonState() {
+        if UserDefaultsConfiguration.sharedUserDefaultConfig.isSoundOn() {
+            UserDefaultsConfiguration.sharedUserDefaultConfig.setSoundToFalse()
+        } else {
+            UserDefaultsConfiguration.sharedUserDefaultConfig.setSoundToTrue()
+
+        }
+    }
+
+}
